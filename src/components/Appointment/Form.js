bdab979a542afa9106107982647aeb096295dd0b
Lliam McBean <lliam.mcbean@gmail.com>
2021-09-17 14:00:53 -0600

    fixed error with transition functionality. having problems running start script on host maching but more likely due to a newer version of node being run

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -62,7 +62,7 @@ export default function Form(props) {
                   }
                 })
                 .catch(() => {
-                  props.transition('ERROR_SAVE', true);
+                  props.transition('ERROR_SAVE');
                 });
             }}
           >
